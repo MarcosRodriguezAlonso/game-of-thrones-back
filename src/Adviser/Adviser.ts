afba3ee816ebb1bc5dee01e11605407e3bcbd6c1
Marcos Rodríguez <e.marcosra@gmail.com>
2024-05-01 15:10:43 +0200

    Create class for Adviser

---
+++ src/Adviser/Adviser.ts
@@ -0,0 +1,15 @@
+import { Character } from "../Character/Character.js";
+import { type BasicCharacter, type AdviserStructure } from "../types.js";
+
+export class Adviser extends Character implements AdviserStructure {
+  public sentence = "No sé por qué, pero creo que voy a morir pronto";
+  public characterWhomAdvises: Character;
+
+  public constructor(
+    character: BasicCharacter,
+    characterWhomAdvises: Character,
+  ) {
+    super(character.name, character.surname, character.age);
+    this.characterWhomAdvises = characterWhomAdvises;
+  }
+}
